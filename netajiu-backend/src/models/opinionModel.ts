934e28434c10f06bb335dfc10c32a7eca4f32748
Grace-Rasaily780 <rasailygrace56@gmail.com>
2024-11-15 21:12:20 +0545

    model completed

--- netajiu-backend/src/models/opinionModel.ts
+++ netajiu-backend/src/models/opinionModel.ts
@@ -3,7 +3,21 @@ import mongoose, { Schema } from "mongoose";
 const opinionschema = new Schema({
   neta:{ type: Schema.Types.ObjectId, ref: "Neta" },
   content: Schema.Types.String,
-  contributor: { type: Schema.Types.ObjectId, ref: "Contributor" }
+  contributor: { type: Schema.Types.ObjectId, ref: "Contributor" },
+  links: [{
+    label: Schema.Types.String,
+    link: Schema.Types.String
+  }],
+  stats: {
+    up: Schema.Types.Number,
+    down: Schema.Types.Number
+  },
+  flag: { type: Schema.Types.ObjectId, ref: "Flag" },
+  tag: { type: Schema.Types.ObjectId, ref: "Tag" },
+  version: {
+    value: Schema.Types.ObjectId
+    versions: [{ type: Schema.Types.ObjectId, ref: "OpinionVersion" }]
+  }
  });
 
 export default mongoose.model("Opinion", opinionschema);
