ad6ca738375a86e0649ccbb9a1092c3df0b75059
prasunghimireofficial <prasunghimire05@gmail.com>
2024-11-15 17:35:50 +0545

    Netaji Intro

---
+++ netajiu-landing/app/neta/page.tsx
@@ -0,0 +1,13 @@
+import NetajiIntro from './../../components/netajiIntro';
+
+const Home = () => {
+  return (
+    <div>
+      {/* <h1>Home Page</h1> */}
+
+      <NetajiIntro />
+    </div>
+  );
+};
+
+export default Home;
