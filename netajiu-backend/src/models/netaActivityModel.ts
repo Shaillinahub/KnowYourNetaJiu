eda7348f5523a09dd3aa5d9da85208a63c673a1d
Grace-Rasaily780 <rasailygrace56@gmail.com>
2024-11-15 21:56:04 +0545

    backed new api feature base

--- netajiu-backend/src/models/netaActivityModel.ts
+++ netajiu-backend/src/models/netaActivityModel.ts
@@ -1,14 +1,9 @@
 import mongoose, { Schema } from "mongoose";
 
 const netaActivityschema = new Schema({
-  fullname: Schema.Types.String,
-  dob: {
-    date: Schema.Types.Date,
-    age: Schema.Types.Number
-  },
-  party: { type: Schema.Types.ObjectId, ref: "Party" },
-  summary: Schema.Types.String,
-  activity: [{ type: Schema.Types.ObjectId, ref: "NetaActivity" }],
+  date: Schema.Types.Date,
+  activity: Schema.Types.String,
+  ref: Schema.Types.String
 });
 
 export default mongoose.model("Neta", netaActivityschema);
