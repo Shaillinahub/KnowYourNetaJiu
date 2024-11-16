bd82863870c943c1e98dfd3390d64745a4fc21aa
Grace-Rasaily780 <rasailygrace56@gmail.com>
2024-11-16 16:52:14 +0545

    Front client sync

--- netajiu-client/src/App.tsx
+++ netajiu-client/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import ContributorProfile from "./pages/ContributorProfile";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <ContributorProfile />
+    </div>
+  );
 }
 
 export default App;
