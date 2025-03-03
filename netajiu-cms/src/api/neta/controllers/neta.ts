e1bad9d6060e70e5665fcc13ada7f9c64dc836df
Grace-Rasaily780 <rasailygrace56@gmail.com>
2025-03-03 15:18:00 +0545

    feat:Strapi-CMS initiated

---
+++ netajiu-cms/src/api/neta/controllers/neta.ts
@@ -0,0 +1,7 @@
+/**
+ * neta controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::neta.neta');
---
+++ netajiu-cms/src/api/neta/routes/neta.ts
@@ -0,0 +1,7 @@
+/**
+ * neta router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::neta.neta');
